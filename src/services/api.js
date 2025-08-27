950d7638e0a8ff896ca7ce775f07b4f02a750239
luis505050 <luisferna267@gmail.com>
2025-08-27 18:48:40 -0500

    Actualizar la baseURL al backend Render

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // URL de tu backend
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // si necesitas cookies
+  withCredentials: true,
 });
 
 export default api;
